d4951603f2204e39081a02ccd103be8c36828193
Lucas da Rosa Nunes <llucas-nunes@hotmail.com>
2024-10-27 11:52:04 -0300

    Realizado código para request

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-import e from "express";
+import express from "express";
 
-const app = e();
+const app = express();
+app.use(express.json())
+
 
 // Métodos HTTP´s, Endpoints Users, request e responses
+const users = []
+app.post("/users", (req, res) => {
+  console.log(req.body)
+  users.push(req.body)
+  res.send("Request funcionou!");
+});
+
 app.get("/users", (req, res) => {
-  res.send("Response funcionou!");
+  res.json(users);
 });
 
 app.listen(3000, () => {
-  console.log('--> O server está de pé!!! :DD')
+  console.log("--> O server está de pé!!! :DD");
 }); //Localhost do app, se necessário pode trocar a porta
